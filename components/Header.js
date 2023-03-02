2933df1253e7eaee5d3f5ec18bd3dc920c08f8fc
Dyrits <dyrtis@outlook.com>
2023-03-02 12:17:17 +0100

    Started "todo-app" project

---
+++ components/Header.js
@@ -0,0 +1,23 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export default function Header() {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.title}>To-do list</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    height: 80,
+    paddingTop: 38,
+    backgroundColor: "coral",
+  },
+  title: {
+    textAlign: "center",
+    color: "#FFF",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
